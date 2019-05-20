bd8581f32d525dafde8c30ecc93f12c7fef9d449
Alexander Miller <alexanderm@web.de>
2019-05-20 21:41:34 +0200

    Basic foundation.

---
+++ pfr.el
@@ -0,0 +1,76 @@
+;;; pfr.el --- pos-frame-based read-string -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Alexander Miller
+
+;; Author: Alexander Miller <alexanderm@web.de>
+;; Package-Requires: ((emacs "25.2") (dash "2.11.0") (s "1.10.0") (posframe "0.4.3"))
+;; Package-Version: 0.1
+;; Homepage: https://github.com/Alexander-Miller/treemacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; TODO
+
+;;; Code:
+
+(require 's)
+(require 'dash)
+(require 'posframe)
+
+(defvar pfr--slot nil)
+(defvar-local pfr--prompt-ov nil)
+
+(defun pfr-read (prompt)
+  "Read a string using a pos-frame.
+TODO PROMPT"
+  (-let [buffer (get-buffer-create "*pfcr*")]
+    (posframe-show buffer
+      :height 1
+      :width (+ 40 (length prompt))
+      :internal-border-width 1
+      :string "")
+    (-let [posfr (-> buffer (get-buffer-window :all-frame) (window-frame))]
+      (x-focus-frame posfr)
+      (with-current-buffer buffer
+        (display-line-numbers-mode -1)
+        (pfr-input-mode)
+        (-let [ov (make-overlay 1 2)]
+          (overlay-put ov 'before-string prompt)
+          (overlay-put ov 'rear-nonsticky t)
+          (overlay-put ov 'read-only t))
+        (recursive-edit)
+        pfr--slot))))
+
+(defun pfr-finish ()
+  "TODO."
+  (interactive)
+  (let ((parent (frame-parent (selected-frame)))
+        (txt (buffer-string)))
+    (posframe-hide (current-buffer))
+    (x-focus-frame parent)
+    (setq pfr--slot txt)
+    (exit-recursive-edit)))
+
+(defvar pfr-input-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") #'pfr-finish)
+    map))
+
+(define-derived-mode pfr-input-mode fundamental-mode "Pos Frame Completing Read"
+  "TODO DOCS")
+
+(provide 'pfr)
+
+;;; pfr.el ends here
