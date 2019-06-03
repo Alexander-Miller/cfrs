809cdc642a18f7c2de34bdfb6ad78b2d577dbc09
Alexander Miller <alexanderm@web.de>
2019-06-03 19:31:13 +0200

    Fix homepage header.
    Fixes #2

--- cfrs.el
+++ cfrs.el
@@ -5,7 +5,7 @@
 ;; Author: Alexander Miller <alexanderm@web.de>
 ;; Package-Requires: ((emacs "25.2") (dash "2.11.0") (s "1.10.0") (posframe "0.4.3"))
 ;; Package-Version: 1.0
-;; Homepage: https://github.com/Alexander-Miller/treemacs
+;; Homepage: https://github.com/Alexander-Miller/cfrs
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
